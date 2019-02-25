6eaf09d6bd54f13a5415808e2e2f65d4a98ccd33
John Williams <johnnjit7@gmail.com>
2019-02-25 00:19:43 -0500

    Installed multer npm package

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({storage: storage});
 
 var app = express();
 
@@ -16,6 +19,10 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next) {
+  res.json({textbody: req.file});
 });
 
 app.listen(process.env.PORT || 3000, function () {
